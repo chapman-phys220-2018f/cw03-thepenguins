aaaead6094465edc2fa7f5f104e1d2bbdb258acb
Jack Savage <jsavage@chapman.edu>
2018-09-13 17:16:47 +0000

    added IndexError handling on fib.py

--- fib.py
+++ fib.py
@@ -1,17 +1,21 @@
 #!/usr/bin/env python3 
 #-*- coding: utf-8 -*-
 ###
-# Name: Jack Savage Student ID: 2295072 Email: jsavage@chapman.edu Course: 
-# PHYS220/MATH220/CPSC220 Fall 2018 Assignment: CW03
+# Name: Jack Savage 
+# Student ID: 2295072 
+# Email: jsavage@chapman.edu 
+# Course: PHYS220/MATH220/CPSC220 Fall 2018 Assignment: CW03
 ###
 def main(local_argv):
     from sequences import fibonacci as fib
-
-    print("This program imports the sequences module, reads a command line"+
-          " argument 'n', and returns the 'n'th fibonacci number")
-
+    
+    
     # storing first value in list of command line arguments for use in sequence
-    n = int(local_argv[1])
+    # catching index error in case of no command line option
+    try:
+        n = int(local_argv[1])
+    except IndexError:
+        n = 1
 
     print(fib(n).pop())
 
